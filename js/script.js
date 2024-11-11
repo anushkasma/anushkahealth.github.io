470ca05c8385ea235db3377ae94f627c4e7866c6
anushkasma <104641846+anushkasma@users.noreply.github.com>
2024-11-11 13:27:24 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Add a click event listener to the "Book a Session" button
+document.querySelector('.button').addEventListener('click', function() {
+    // Redirect to the booking page
+    window.location.href = 'https://www.carewave.com/book-a-session';
+  });
+  
+  // Add a click event listener to the "Learn More" button
+  document.querySelector('.learn-more').addEventListener('click', function() {
+    // Redirect to the learn more page
+    window.location.href = 'https://www.carewave.com/learn-more';
+  });
